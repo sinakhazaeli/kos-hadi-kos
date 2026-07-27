75fc23caa1e7f649dc0e225d9a0905348d5a6aaa
sinakhazaeli <khazaeli.sina@gmail.com>
2026-07-27 23:44:40 +0330

    Add files via upload
        Update game to version 0.2

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='khk-v02';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
